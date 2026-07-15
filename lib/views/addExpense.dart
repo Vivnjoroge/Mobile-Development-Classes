ccf4a99bb5dd9bb1851850c9e3d27e33067a90f3
Vivian Njoroge <viviannjoroge91@gmail.com>
2026-07-15 12:19:10 +0300

    add views

---
+++ lib/views/addExpense.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Addexpense extends StatefulWidget {
+  const Addexpense({super.key});
+
+  @override
+  State<Addexpense> createState() => _AddexpenseState();
+}
+
+class _AddexpenseState extends State<Addexpense> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/views/expenseDetails.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Expensedetails extends StatefulWidget {
+  const Expensedetails({super.key});
+
+  @override
+  State<Expensedetails> createState() => _ExpensedetailsState();
+}
+
+class _ExpensedetailsState extends State<Expensedetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
